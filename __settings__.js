5e4e53ca184070f379f6be8e207a5f1609bcb98d
OmN3xin <broleksyn@gmail.com>
2021-05-21 12:14:24 -0400

    1.0

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1057518.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 40209426, 40209471, 40209699, 40210681 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/40209183/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/40209184/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/40209182/1/ammo.js', 'preload' : true},
+];
